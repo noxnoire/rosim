d961cb98201040666d3c1c177e46ac555acbff28
noxnoire <noxnoire@gmail.com>
2020-07-31 08:54:37 -0400

    Update ninja.js
    more prettier fixes

--- src/constants/classes/ninja.js
+++ src/constants/classes/ninja.js
@@ -1,4 +1,4 @@
-import {  KAGERO_OBORO, NINJA, TRANSCENDENT_SECOND } from './classNames';
+import { KAGERO_OBORO, NINJA, TRANSCENDENT_SECOND } from './classNames';
 
 export default {
   value: NINJA,
@@ -7,7 +7,7 @@ export default {
     {
       value: KAGERO_OBORO,
       type: TRANSCENDENT_SECOND,
-      label: 'KAGERO_OBORO',
-    },
-  ],
-};
+      label: 'KAGERO_OBORO'
+    }
+  ]
+;
