69aad11e1e4f2065dbb48a63ce0fae6c1f1de8df
noxnoire <noxnoire@gmail.com>
2020-07-31 09:16:43 -0400

    Update ranges.js
    fixing ranges

--- src/constants/ranges.js
+++ src/constants/ranges.js
@@ -6,7 +6,7 @@ import { getJobType } from './classes';
 const jobLevelMap = {
   [SECOND]: 50,
   [TRANSCENDENT_SECOND]: 70,
-  [THIRD]: 60,
+  [THIRD]: 13,
 };
 
 export const getMaxBaseLevel = job =>
@@ -16,6 +16,6 @@ export const getMaxStats = job =>
   getJobType(job) === TRANSCENDENT_SECOND ? 120 : 99;
 
 export const getBaseLevelRange = job =>
-  getJobType(job) === THIRD ? range(99, 176) : range(1, 100);
+  getJobType(job) === TRANSCENDENT_SECOND ? range(1, 121) : range(1, 100);
 export const getJobLevelRange = job => range(1, getMaxJobLevel(job) + 1);
 export const getStatsRange = job => range(1, getMaxStats(job) + 1);
