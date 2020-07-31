a0a66123f52c89648c915d244fab97789e8fe6eb
noxnoire <noxnoire@gmail.com>
2020-07-31 08:23:44 -0400

    Update ranges.js
    prettier fixes

--- src/constants/ranges.js
+++ src/constants/ranges.js
@@ -1,7 +1,7 @@
-import { range } from 'lodash';
+import { range } from "lodash";
 
-import { SECOND, THIRD, TRANSCENDENT_SECOND } from './classes/classNames';
-import { getJobType } from './classes';
+import { SECOND, THIRD, TRANSCENDENT_SECOND } from "./classes/classNames";
+import { getJobType } from "./classes";
 
 const jobLevelMap = {
   [SECOND]: 50,
@@ -9,12 +9,13 @@ const jobLevelMap = {
   [THIRD]: 60,
 };
 
-export const getMaxBaseLevel = job => getJobType(job) === TRANSCENDENT_SECOND ? 120 : 99;
-export const getMaxJobLevel = job => jobLevelMap[getJobType(job)];
-export const getMaxStats = job => getJobType(job) === TRANSCENDENT_SECOND ? 120 : 99;
+export const getMaxBaseLevel = (job) =>
+  getJobType(job) === TRANSCENDENT_SECOND ? 120 : 99;
+export const getMaxJobLevel = (job) => jobLevelMap[getJobType(job)];
+export const getMaxStats = (job) =>
+  getJobType(job) === TRANSCENDENT_SECOND ? 120 : 99;
 
-
-export const getBaseLevelRange = job =>
+export const getBaseLevelRange = (job) =>
   getJobType(job) === THIRD ? range(99, 176) : range(1, 100);
-export const getJobLevelRange = job => range(1, getMaxJobLevel(job) + 1);
-export const getStatsRange = job => range(1, getMaxStats(job) + 1);
+export const getJobLevelRange = (job) => range(1, getMaxJobLevel(job) + 1);
+export const getStatsRange = (job) => range(1, getMaxStats(job) + 1);
